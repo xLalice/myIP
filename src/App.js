07fd7d8d9a2de406a3a6a311533792ac0b794f34
gtcore902 <gaetan.tremois@gmail.com>
2024-03-22 19:51:02 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import { useState, useEffect } from 'react';
+import './App.scss';
+
+function App() {
+  const [ip, setIp] = useState('');
+  const [geolocatioDatas, setGeolocationDatas] = useState('');
+
+  const fetchIp = async () => {
+    try {
+      const response = await fetch('http://localhost:4000');
+      const data = await response.json();
+      console.log(data);
+      setIp(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getGeolocationDatas = async (ip) => {
+    try {
+      const response = await fetch(`http://ip-api.com/json/${ip}`);
+      const data = await response.json();
+      console.log(data);
+      setGeolocationDatas(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchIp();
+  }, []);
+
+  useEffect(() => {
+    getGeolocationDatas(ip.ip);
+  }, [ip]);
+
+  return (
+    <div className="main">
+      <h1>Get your IP Address</h1>
+      <p className="main__content">
+        Your IP address : <span className="main__content--datas">{ip.ip}</span>
+      </p>
+      <p className="main__content">
+        City :{' '}
+        <span className="main__content--datas">{geolocatioDatas.city}</span>
+      </p>
+      <p className="main__content">
+        Country :{' '}
+        <span className="main__content--datas">{geolocatioDatas.country}</span>
+      </p>
+      <p className="main__content">
+        Lat :{' '}
+        <span className="main__content--datas">{geolocatioDatas.lat}</span>
+      </p>
+      <p className="main__content">
+        Lon :{' '}
+        <span className="main__content--datas">{geolocatioDatas.lon}</span>
+      </p>
+      <p className="main__content">
+        Région :{' '}
+        <span className="main__content--datas">
+          {geolocatioDatas.regionName}
+        </span>
+      </p>
+    </div>
+  );
+}
+
+export default App;
