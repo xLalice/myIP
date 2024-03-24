833076223c72650fe475f6befbc6f816db50df54
gtcore902 <gaetan.tremois@gmail.com>
2024-03-24 09:32:27 +0100

    feat: implemente geolocation API

--- src/App.js
+++ src/App.js
@@ -6,10 +6,27 @@ import './_shared.scss';
 
 function App() {
   const [ip, setIp] = useState('');
-  const [geolocatioDatas, setGeolocationDatas] = useState('');
-  const [lat, setLat] = useState('');
-  const [lon, setLon] = useState('');
+  const [IpDatas, setIpDatas] = useState('');
+  const [IpLat, setIpLat] = useState('');
+  const [IpLon, setIpLon] = useState('');
+  const [geolocationCity, setGeolocationCity] = useState([]);
+  const [geoLat, setGeoLat] = useState('');
+  const [geoLon, setGeoLon] = useState('');
 
+  /**
+   * Get location information from geolocation API
+   */
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // console.log(position.coords.latitude, position.coords.longitude);
+      // console.log(position);
+      setGeoLat(position.coords.latitude);
+      setGeoLon(position.coords.longitude);
+      // getGeolocationDatas(position.coords.latitude, position.coords.longitude);
+    });
+  } else {
+    console.log('no');
+  }
   /**
    * Fetch backend to get IP informations
    */
@@ -17,7 +34,7 @@ function App() {
     try {
       const response = await fetch('http://localhost:4000');
       const data = await response.json();
-      console.log(data);
+      // console.log(data);
       setIp(data.ip);
     } catch (error) {
       console.log(error);
@@ -28,19 +45,39 @@ function App() {
    * Fetch geolocation datas with IP address
    * @param {string} ip
    */
-  const getGeolocationDatas = async (ip) => {
+  const getIpDatas = async (ip) => {
     try {
       const response = await fetch(`http://ip-api.com/json/${ip}`);
       const data = await response.json();
-      console.log(data);
-      setGeolocationDatas(data);
-      setLat(data.lat);
-      setLon(data.lon);
+      // check errors here
+      setIpDatas(data);
+      setIpLat(data.lat);
+      setIpLon(data.lon);
     } catch (error) {
       console.log(error);
     }
   };
 
+  const getGeolocationDatas = async (geoLat, geoLon) => {
+    try {
+      const response = await fetch(
+        `http://api.openweathermap.org/geo/1.0/reverse?lat=${geoLat}&lon=${geoLon}&appid=4e02788426486030d33ca69dde69af9d`
+      );
+      const data = await response.json();
+      if (response.status === 200) {
+        setGeolocationCity(data[0].name);
+      }
+      if (response.status === 429) {
+        setGeolocationCity('unvailable');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /**
+   * Fetch backend to get location from IP address
+   */
   useEffect(() => {
     fetchIp();
   }, []);
@@ -49,8 +86,15 @@ function App() {
    * Launch geolocation datas fetch when ip is set
    */
   useEffect(() => {
-    getGeolocationDatas(ip);
+    getIpDatas(ip);
   }, [ip]);
+
+  /**
+   * If geolocation API authorized fetch reverse location API to get city name
+   */
+  useEffect(() => {
+    (geoLat !== '') & (geoLon !== '') && getGeolocationDatas(geoLat, geoLon);
+  }, [geoLat, geoLon]);
 
   return (
     <div className="main">
@@ -62,31 +106,35 @@ function App() {
         </p>
         <p className="datas__content--label">
           City :{' '}
-          <span className="datas__content--data">{geolocatioDatas.city}</span>
+          <span className="datas__content--data">
+            {IpDatas.city} - {geolocationCity}
+          </span>
         </p>
         <p className="datas__content--label">
           Country :{' '}
-          <span className="datas__content--data">
-            {geolocatioDatas.country}
-          </span>
+          <span className="datas__content--data">{IpDatas.country}</span>
         </p>
         <p className="datas__content--label">
           Lat :{' '}
-          <span className="datas__content--data">{geolocatioDatas.lat}</span>
+          <span className="datas__content--data">
+            {IpDatas.lat} - {geoLat}
+          </span>
         </p>
         <p className="datas__content--label">
           Lon :{' '}
-          <span className="datas__content--data">{geolocatioDatas.lon}</span>
+          <span className="datas__content--data">
+            {IpDatas.lon} - {geoLon}
+          </span>
         </p>
         <p className="datas__content--label">
           Région :{' '}
-          <span className="datas__content--data">
-            {geolocatioDatas.regionName}
-          </span>
+          <span className="datas__content--data">{IpDatas.regionName}</span>
         </p>
       </div>
       {/* Check if lat and lon is defined */}
-      {(lat !== '') & (lon !== '') ? <Map lat={lat} lon={lon} /> : null}
+      {(IpLat !== '') & (IpLon !== '') && (
+        <Map IpLat={IpLat} IpLon={IpLon} geoLat={geoLat} geoLon={geoLon} />
+      )}
       <Footer />
     </div>
   );
--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,17 +1,30 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './Map.scss';
 
-const Map = ({ lat, lon }) => {
+const Map = ({ IpLat, IpLon, geoLat, geoLon }) => {
+  let position = [];
+  if (geoLat & geoLon) {
+    const averageLat = (IpLat + geoLat) / 2;
+    const averageLon = (IpLon + geoLon) / 2;
+    position = [averageLat, averageLon];
+  } else {
+    position = [IpLat, IpLon];
+  }
   return (
     <div id="map">
-      <MapContainer center={[lat, lon]} zoom={9} scrollWheelZoom={true}>
+      <MapContainer center={position} zoom={9} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[lat, lon]}>
-          <Popup>Your current position.</Popup>
+        <Marker position={[IpLat, IpLon]}>
+          <Popup>Your current IP position.</Popup>
         </Marker>
+        {(geoLat !== '') & (geoLon !== '') && (
+          <Marker position={[geoLat, geoLon]} style={{ color: 'red' }}>
+            <Popup>Your current geolocation position.</Popup>
+          </Marker>
+        )}
       </MapContainer>
     </div>
   );
