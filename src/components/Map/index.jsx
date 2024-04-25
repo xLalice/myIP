e573213d205c0f84d199962234ae14d85ff48007
gtcore902 <gaetan.tremois@gmail.com>
2024-04-25 17:12:26 +0200

    feat: increase map zoom

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -20,7 +20,7 @@ const Map = ({ IpLat, IpLon, geoLat, geoLon }) => {
   }
   return (
     <div id="map">
-      <MapContainer center={position} zoom={8} scrollWheelZoom={true}>
+      <MapContainer center={position} zoom={9} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
